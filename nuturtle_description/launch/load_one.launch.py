e5436f0f8ddfab40bd84947660f446286ae91c91
Nader Ahmed <ndr.ahmed1@gmail.com>
2024-01-05 23:47:41 -0600

    Multi colors are working
    - made the fixed frame the color arg name in the launch file
    - updated the namespaces
    - added all the color config files, updated the tf frame prefixes
    - the frame prefix for the robot state pub is setup

--- nuturtle_description/launch/load_one.launch.py
+++ nuturtle_description/launch/load_one.launch.py
@@ -1,8 +1,8 @@
 """ Loads the turtlebot3_burger URDF and allows it to be viewed in rviz. """
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
+from launch.actions import DeclareLaunchArgument, SetLaunchConfiguration
 from launch_ros.actions import Node
-from launch.substitutions import Command, PathJoinSubstitution, LaunchConfiguration
+from launch.substitutions import Command, PathJoinSubstitution, LaunchConfiguration, TextSubstitution
 from launch_ros.substitutions import FindPackageShare, ExecutableInPackage
 from launch.conditions import IfCondition
 from launch.substitutions import EqualsSubstitution
@@ -17,6 +17,14 @@ def generate_launch_description():
         default_value="purple",
         choices=['red', 'blue', 'purple', 'green'],
         description='Color choice for the turtle bot. purple by default'
+    ),
+    
+    SetLaunchConfiguration(
+        name="rviz_config",
+        value=[FindPackageShare("nuturtle_description"), 
+            TextSubstitution(text="/config/basic_"),
+            LaunchConfiguration("color"),
+            TextSubstitution(text=".rviz")]
     ),
     
     # use_jsp launch argument
@@ -37,6 +45,7 @@ def generate_launch_description():
     
     # joint_state_publisher node
     Node(
+        namespace=LaunchConfiguration("color"),
         package="joint_state_publisher",
         executable="joint_state_publisher",
         condition=IfCondition(EqualsSubstitution(LaunchConfiguration("use_jsp"), "true")),
@@ -44,24 +53,30 @@ def generate_launch_description():
     
     # robot_state_publisher node, loading in the turtlebot3_burger urdf
     Node(
+        namespace=LaunchConfiguration("color"),
         package="robot_state_publisher",
         executable="robot_state_publisher",
         parameters=[
             {"robot_description": 
-                Command([ExecutableInPackage("xacro", "xacro"), " ",
-                        PathJoinSubstitution(
-                    [FindPackageShare("nuturtle_description"), \
-                     "urdf/turtlebot3_burger.urdf.xacro"])]
-            )}
+                Command([
+                    ExecutableInPackage("xacro", "xacro"), " ",
+                    PathJoinSubstitution(
+                    [FindPackageShare("nuturtle_description"), 
+                        "urdf/turtlebot3_burger.urdf.xacro"]),
+                    " color:=", LaunchConfiguration("color")]),    
+            "frame_prefix":PathJoinSubstitution([(LaunchConfiguration('color')), '']),
+            }
         ]
     ),
     
     # rviz2 node
     Node(
+        namespace=LaunchConfiguration("color"),
         package="rviz2",
         executable="rviz2",
         condition=IfCondition(EqualsSubstitution(LaunchConfiguration("use_rviz"), "true")),
-        on_exit=launch.actions.Shutdown()
-    )
-    
-    ])
+        on_exit=launch.actions.Shutdown(),
+        arguments=[
+            "-d", LaunchConfiguration("rviz_config"),
+            "-f", PathJoinSubstitution([LaunchConfiguration("color"), "base_link"])]
+    )])
