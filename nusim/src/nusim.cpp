dee61edfabb4008e6558473174146b12130f00b4
Nader Ahmed <ndr.ahmed1@gmail.com>
2024-01-14 14:46:34 -0600

    Timer setup, reset service implemented and working [x] setup timer with 200hz callback frequency [x] setup timestep counter [x] implemented reset service for timestep counter [x] handled the "unused param" issues by removing param name [x] added the std_srvs/Trigger.srv and the std_srvs dependency in CMakeLists.txt

--- nusim/src/nusim.cpp
+++ nusim/src/nusim.cpp
@@ -1,5 +1,7 @@
 #include "rclcpp/rclcpp.hpp"
-
+#include "std_msgs/msg/string.hpp"
+#include "std_msgs/msg/u_int64.hpp"
+#include "std_srvs/srv/trigger.hpp"
 // https://docs.ros.org/en/iron/Tutorials/Beginner-Client-Libraries/Writing-A-Simple-Cpp-Publisher-And-Subscriber.html#write-the-subscriber-node
 using namespace std::chrono_literals;
 
@@ -7,24 +9,47 @@ class NUSim : public rclcpp::Node
 {
 
 public:
-    NUSim(): Node("nusim") { //constructor
+    NUSim() : Node("nusim")
+    { 
+        timestep = 0;
+        timestep_pub = create_publisher<std_msgs::msg::UInt64>("~/timestep", 10);
+        reset_srv = create_service<std_srvs::srv::Trigger>("~/reset",
+                                                           std::bind(&NUSim::reset_callback,
+                                                                     this, std::placeholders::_1,
+                                                                     std::placeholders::_2));
 
-    declare_parameter("nusim.nu", 0.0);
-
-
-    RCLCPP_INFO(this->get_logger(), "hello world %d", 4);
-    RCLCPP_INFO_STREAM(this->get_logger(), "helo world " << 4);
-
+        declare_parameter("rate", 200.0);
+        rate = get_parameter("rate").as_double();
+        timer = create_wall_timer(1s / rate, std::bind(&NUSim::timer_callback, this));
     }
 
-
-
 private:
+    rclcpp::TimerBase::SharedPtr timer;
+    rclcpp::Publisher<std_msgs::msg::UInt64>::SharedPtr timestep_pub;
+    rclcpp::Service<std_srvs::srv::Trigger>::SharedPtr reset_srv;
 
+    double rate;
+    uint64_t timestep;
 
+    void timer_callback()
+    {
+        std_msgs::msg::UInt64 msg;
+
+        timestep++;
+        msg.data = timestep;
+        timestep_pub->publish(msg);
+    }
+
+    void reset_callback(const std::shared_ptr<std_srvs::srv::Trigger::Request>,
+                        std::shared_ptr<std_srvs::srv::Trigger::Response> response)
+    {
+        RCLCPP_INFO_STREAM(this->get_logger(), "resetting timer");
+        timestep = 0;
+        response->success = true;
+    }
 };
 
-int main(int argc, char * argv[])
+int main(int argc, char *argv[])
 {
     rclcpp::init(argc, argv);
     auto node = std::make_shared<NUSim>();
