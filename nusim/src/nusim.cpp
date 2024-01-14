c85029c58ee62fd74c77c1776cc48a4c8f2b732a
Nader Ahmed <ndr.ahmed1@gmail.com>
2024-01-14 17:33:29 -0600

    Target name for interface file generation fixed ------------------------------------------ [x] issue with the target name being the same as the project name [x] teleport.hpp includable in the nusim node [x] transform broadcaster set up for the red base footprint

--- nusim/src/nusim.cpp
+++ nusim/src/nusim.cpp
@@ -2,7 +2,10 @@
 #include "std_msgs/msg/string.hpp"
 #include "std_msgs/msg/u_int64.hpp"
 #include "std_srvs/srv/trigger.hpp"
-// https://docs.ros.org/en/iron/Tutorials/Beginner-Client-Libraries/Writing-A-Simple-Cpp-Publisher-And-Subscriber.html#write-the-subscriber-node
+#include "tf2_ros/transform_broadcaster.h"
+#include "tf2/LinearMath/Quaternion.h"
+#include "nusim/srv/teleport.hpp"
+
 using namespace std::chrono_literals;
 
 class NUSim : public rclcpp::Node
@@ -12,14 +15,26 @@ public:
     NUSim() : Node("nusim")
     { 
         timestep = 0;
+
+        tf_broadcaster = std::make_unique<tf2_ros::TransformBroadcaster>(*this);
+
         timestep_pub = create_publisher<std_msgs::msg::UInt64>("~/timestep", 10);
         reset_srv = create_service<std_srvs::srv::Trigger>("~/reset",
                                                            std::bind(&NUSim::reset_callback,
                                                                      this, std::placeholders::_1,
                                                                      std::placeholders::_2));
-
         declare_parameter("rate", 200.0);
+        declare_parameter("x0", 0.0);
+        declare_parameter("y0", 0.0);
+        declare_parameter("theta0", 0.0);
+
         rate = get_parameter("rate").as_double();
+        x0 = get_parameter("x0").as_double();
+        y0 = get_parameter("y0").as_double();
+        theta0 = get_parameter("theta0").as_double();
+
+        cur_x = x0, cur_y = y0, cur_theta = theta0;
+
         timer = create_wall_timer(1s / rate, std::bind(&NUSim::timer_callback, this));
     }
 
@@ -27,26 +42,43 @@ private:
     rclcpp::TimerBase::SharedPtr timer;
     rclcpp::Publisher<std_msgs::msg::UInt64>::SharedPtr timestep_pub;
     rclcpp::Service<std_srvs::srv::Trigger>::SharedPtr reset_srv;
+    std::unique_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster;
+    geometry_msgs::msg::TransformStamped xform_stamped;
+    tf2::Quaternion q;
 
+    double x0, y0, theta0, cur_x, cur_y, cur_theta;
     double rate;
     uint64_t timestep;
-
+ 
     void timer_callback()
     {
         std_msgs::msg::UInt64 msg;
-
         timestep++;
         msg.data = timestep;
         timestep_pub->publish(msg);
+
+        xform_stamped.header.stamp = this->get_clock()->now();
+        xform_stamped.header.frame_id = "nusim/world";
+        xform_stamped.child_frame_id = "red/base_footprint";
+        xform_stamped.transform.translation.x = cur_x;
+        xform_stamped.transform.translation.y = cur_y;
+        xform_stamped.transform.translation.z = 0.0;
+        q.setRPY(0, 0, cur_theta);
+        xform_stamped.transform.rotation.x = q.x();
+        xform_stamped.transform.rotation.y = q.y();
+        xform_stamped.transform.rotation.z = q.z();
+        xform_stamped.transform.rotation.w = q.w();
+        tf_broadcaster->sendTransform(xform_stamped);
     }
 
     void reset_callback(const std::shared_ptr<std_srvs::srv::Trigger::Request>,
                         std::shared_ptr<std_srvs::srv::Trigger::Response> response)
     {
-        RCLCPP_INFO_STREAM(this->get_logger(), "resetting timer");
-        timestep = 0;
+        RCLCPP_INFO_STREAM(this->get_logger(), "resetting timestep");
+        cur_x = x0, cur_y = y0, cur_theta = theta0, timestep = 0;
         response->success = true;
     }
+    
 };
 
 int main(int argc, char *argv[])
