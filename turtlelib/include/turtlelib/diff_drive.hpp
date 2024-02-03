80c6cdc27cabba7609283c0004cc86079a1f24af
Nader Ahmed <ndr.ahmed1@gmail.com>
2024-02-03 00:59:36 -0600

    diff drive cleanup

--- turtlelib/include/turtlelib/diff_drive.hpp
+++ turtlelib/include/turtlelib/diff_drive.hpp
@@ -3,45 +3,48 @@
 #include "turtlelib/geometry2d.hpp"
 #include "turtlelib/se2d.hpp"
 
-namespace turtlelib { 
+namespace turtlelib
+{
 
     /// @brief robot configuration
-    struct robotConfig { 
+    struct robotConfig
+    {
         double x{0.0};
         double y{0.0};
         double theta{0.0};
     };
 
     /// @brief wheel velocities
-    struct wheelVel {
+    struct wheelVel
+    {
         double left_wheel_vel{0.0};
         double right_wheel_vel{0.0};
     };
 
-    class DiffDrive {
+    class DiffDrive
+    {
 
     public:
         /// @brief diff drive constructor
-        /// @param wheel_radius 
-        /// @param track_width 
+        /// @param wheel_radius
+        /// @param track_width
         DiffDrive(double wheel_radius, double track_width);
-  
 
         /// @brief forward kinematics. update robot configuration given wheel motion
-        /// @param new_wheel_config 
+        /// @param new_wheel_config
         void forwardKinematics(const wheelVel new_wheel_config);
 
-        //ik: compute the wheel velocities required to achieve a desired twist
-        
+        // ik: compute the wheel velocities required to achieve a desired twist
+
         /// @brief inverse kinematics. compute the wheel velocities required to achieve a desired twist
-        /// @param twist 
+        /// @param twist
         /// @return wheelVel object with left and right wheel velocities
         wheelVel inverseKinematics(const Twist2D twist) const;
 
-        //convert wheel velocities to actual body twis
-        
+        // convert wheel velocities to actual body twis
+
         /// @brief convert wheel velocities to actual body twist
-        /// @param u 
+        /// @param u
         /// @return Twist2D object of {omega, xDot, yDot}
         Twist2D computeBodyTwist(const wheelVel u) const;
 
@@ -51,10 +54,10 @@ namespace turtlelib {
 
     private:
         robotConfig cur_config;
-        wheelVel cur_wheel_vel; 
+        wheelVel cur_wheel_vel;
 
-        //defaults for turtlebot3 burger
-        double wheel_radius{0.033}; 
+        // defaults for turtlebot3 burger
+        double wheel_radius{0.033};
         double track_width{0.16};
     };
 
--- turtlelib/src/diff_drive.cpp
+++ turtlelib/src/diff_drive.cpp
@@ -1,4 +1,4 @@
-#include "turtlelib/diff_drive.hpp" // Assume your header is named diff_drive.hpp
+#include "turtlelib/diff_drive.hpp"
 #include "turtlelib/geometry2d.hpp"
 #include "turtlelib/se2d.hpp"
 #include <stdexcept>
@@ -9,29 +9,23 @@ namespace turtlelib
     DiffDrive::DiffDrive(double wheel_radius, double track_width)
         : wheel_radius(wheel_radius), track_width(track_width) {}
 
-    // assumptions:
-    //  1. both wheels started and stopped rotating simultaneously
-    //  2. both wheels rotate at a constant velocity
-    //  3. the wheels achieved their velocity instantaneously
-    //  4. no wheel slippage
-    //  5. the wheels rotated the shortest distance to achieve the new configuration
     void DiffDrive::forwardKinematics(const wheelVel u)
     {
         Twist2D delta_q, Vb;
 
-        //convert wheel configuration to body twist
+        // convert wheel configuration to body twist
         Vb = computeBodyTwist(u);
 
-        //integrate the body twist to get the new configuration
+        // integrate the body twist to get the new configuration
         Transform2D T_b_bp = integrate_twist(Vb);
 
-        //get the robot location in the world frame
+        // get the robot location in the world frame
         Transform2D T_wb{Vector2D{cur_config.x, cur_config.y}, cur_config.theta};
 
-        //get the new robot location in the world frame
+        // get the new robot location in the world frame
         Transform2D T_w_bp = T_wb * T_b_bp;
 
-        //update the robot configuration
+        // update the robot configuration
         cur_config.x = T_w_bp.translation().x;
         cur_config.y = T_w_bp.translation().y;
         cur_config.theta = normalize_angle(T_w_bp.rotation());
@@ -40,11 +34,11 @@ namespace turtlelib
     Twist2D DiffDrive::computeBodyTwist(const wheelVel u) const
     {
         Twist2D twist;
-        double d{0.5*track_width};
+        double d{0.5 * track_width};
 
-        twist.omega = (wheel_radius/(2*d)) * (u.right_wheel_vel - u.left_wheel_vel);
+        twist.omega = (wheel_radius / (2 * d)) * (u.right_wheel_vel - u.left_wheel_vel);
 
-        twist.x = (wheel_radius/2) * (u.right_wheel_vel + u.left_wheel_vel);
+        twist.x = (wheel_radius / 2) * (u.right_wheel_vel + u.left_wheel_vel);
 
         twist.y = 0.0;
 
@@ -54,7 +48,7 @@ namespace turtlelib
     wheelVel DiffDrive::inverseKinematics(const Twist2D twist) const
     {
         wheelVel u;
-        double d{0.5*track_width};
+        double d{0.5 * track_width};
 
         if (twist.y != 0.0)
         {
@@ -62,8 +56,8 @@ namespace turtlelib
         }
         else
         {
-            u.right_wheel_vel = (1/wheel_radius) * (-d*twist.omega + twist.x);
-            u.left_wheel_vel = (1/wheel_radius) * (d*twist.omega + twist.x);
+            u.right_wheel_vel = (1 / wheel_radius) * (-d * twist.omega + twist.x);
+            u.left_wheel_vel = (1 / wheel_radius) * (d * twist.omega + twist.x);
             return u;
         }
     }
