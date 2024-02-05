a98c3994d5e6c23f63a04310046dc4c4509806bc
Nader Ahmed <ndr.ahmed1@gmail.com>
2024-02-05 02:06:20 -0600

    control, reverse, and stop srvs added need to test

--- nuturtle_control/src/circle.cpp
+++ nuturtle_control/src/circle.cpp
@@ -3,7 +3,6 @@
 #include "nuturtle_control/srv/control.hpp"
 #include "geometry_msgs/msg/twist.hpp"
 
-
 using namespace std::chrono_literals;
 
 class Circle : public rclcpp::Node
@@ -26,7 +25,8 @@ public:
             "/stop", std::bind(&Circle::stop_callback, this, std::placeholders::_1,
                                std::placeholders::_2));
 
-        timer = create_wall_timer(100ms, std::bind(&Circle::timer_callback, this));
+        timer = create_wall_timer(std::chrono::milliseconds(1000 / frequency), 
+                    std::bind(&Circle::timer_callback, this));
 
         cmd_pub = create_publisher<geometry_msgs::msg::Twist>("/cmd_vel", 10);
     }
@@ -36,35 +36,51 @@ private:
     rclcpp::Service<std_srvs::srv::Empty>::SharedPtr reverse_srv;
     rclcpp::Service<std_srvs::srv::Empty>::SharedPtr stop_srv;
     rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr cmd_pub;
-    rclcpp::TimerBase::SharedPtr timer; 
+    rclcpp::TimerBase::SharedPtr timer;
+    geometry_msgs::msg::Twist body_twist;
+
     int frequency;
+    bool stopped{true}; // initialize as true to not publish until a control command is received
 
     void control_callback(const std::shared_ptr<nuturtle_control::srv::Control::Request> request,
                           std::shared_ptr<nuturtle_control::srv::Control::Response>)
     {
-        RCLCPP_INFO_STREAM(get_logger(), "Received request to move in a circle with radius "
-                                             << request->radius);
-        // TODO
+        // based on v = w * r, so w = v / r. (the velocity parameter is the angular in rad/s)
+        auto linear_velocity{request->velocity * request->radius};
+        body_twist.linear.x = linear_velocity;
+        body_twist.angular.z = request->velocity;
+        stopped = false;
+
+        RCLCPP_INFO_STREAM(get_logger(), "Received request to move in a circle with radius " 
+                            << request->radius << " and angular velocity " << request->velocity);
     }
 
     void reverse_callback(const std::shared_ptr<std_srvs::srv::Empty::Request>,
                           std::shared_ptr<std_srvs::srv::Empty::Response>)
     {
+        body_twist.linear.x *= -1;
+        body_twist.angular.z *= -1;
+
         RCLCPP_INFO(get_logger(), "Received request to reverse direction");
-        // TODO
     }
 
     void stop_callback(const std::shared_ptr<std_srvs::srv::Empty::Request>,
                        std::shared_ptr<std_srvs::srv::Empty::Response>)
     {
         RCLCPP_INFO(get_logger(), "Received request to stop");
-        // TODO
+        body_twist.linear.x = 0;
+        body_twist.angular.z = 0;
+        stopped = true;
+        cmd_pub->publish(body_twist); // publish a single cmd_vel command of zero when stopped
     }
 
     void timer_callback()
     {
-        //publish cmd_vel commands at a fixed rate
-        // TODO
+        // only publish if not stopped
+        if (!stopped)
+        {
+            cmd_pub->publish(body_twist);
+        }
     }
 };
 
